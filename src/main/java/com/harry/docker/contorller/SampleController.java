df47dee300c0a15741a5d7561b7e1e849285b186
duan-duan <972065033@qq.com>
2018-08-06 18:12:52 +0800

    add: log4j2

--- src/main/java/com/harry/docker/contorller/SampleController.java
+++ src/main/java/com/harry/docker/contorller/SampleController.java
@@ -1,5 +1,7 @@
 package com.harry.docker.contorller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
@@ -17,14 +19,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+
+    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
+        logger.info("Hello World!");
         return "Hello World!";
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(SampleController.class, args);
     }
 
 }
