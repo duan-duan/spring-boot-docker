13185df28ee0e9428fdaecad43aaff9c68fe28b3
duan-duan <972065033@qq.com>
2018-08-06 15:54:07 +0800

    add: init

---
+++ src/main/java/com/harry/docker/SpringBootDockerApplication.java
@@ -0,0 +1,12 @@
+package com.harry.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/harry/docker/contorller/SampleController.java
@@ -0,0 +1,30 @@
+package com.harry.docker.contorller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * This is Description
+ *
+ * @author: wangruirui
+ * @date: 2018/08/06
+ */
+
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+
+}
